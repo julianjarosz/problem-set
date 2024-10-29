97f30bd644bbefa153f8f9dacb4d86f56f278954
julianjarosz <julek.jarosz123@gmail.com>
2024-10-29 19:56:39 +0100

    Implemented a function that solves an IQ test

---
+++ difference_evenness.py
@@ -0,0 +1,17 @@
+def find_different_evenness(n, numbers):
+    numbers = list(map(int, numbers.split()))
+    
+    # Sprawdzenie parzystości dla wszystkich liczb
+    even_indices = [i + 1 for i in range(n) if numbers[i] % 2 == 0]  # Indeksy liczb parzystych
+    odd_indices = [i + 1 for i in range(n) if numbers[i] % 2 != 0]   # Indeksy liczb nieparzystych
+    
+    # Wybór indeksu, który się różni parzystością
+    if len(even_indices) == 1:
+        return even_indices[0]
+    else:
+        return odd_indices[0]
+
+# Przykład użycia
+n = int(input())
+numbers = input()
+print(find_different_evenness(n, numbers))
