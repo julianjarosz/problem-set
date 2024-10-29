4b83cad8c97b84f67b6c8903e1a178c3163784dc
Julian Jarosz <94641908+julianjarosz@users.noreply.github.com>
2024-10-29 19:58:38 +0100

    Update difference_evenness.py
    Extended the description of the conditional statement

--- difference_evenness.py
+++ difference_evenness.py
@@ -5,7 +5,8 @@ def find_different_evenness(n, numbers):
     even_indices = [i + 1 for i in range(n) if numbers[i] % 2 == 0]  # Indeksy liczb parzystych
     odd_indices = [i + 1 for i in range(n) if numbers[i] % 2 != 0]   # Indeksy liczb nieparzystych
     
-    # Wybór indeksu, który się różni parzystością
+    # Wybór indeksu, który się różni parzystościa
+    #Jeżeli lista ma dlugosc jeden to znaczy ze znalezlismy typ liczby i jej indeks, bo ona stanowi wyjatek w calej liscie
     if len(even_indices) == 1:
         return even_indices[0]
     else:
